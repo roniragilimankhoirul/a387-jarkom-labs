0264fe24ed4cae9a23597d145c5eccbe2e3ac6d3
roniragilimankhoirul <ronisensei7@gmail.com>
2023-09-25 15:47:47 +0700

    change respond from 'Hello World to 'Roni Ragil Iman Khoirul''

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-'use strict'
+"use strict";
 
-const express = require('express')
+const express = require("express");
 
-const PORT = 8000
-const HOST = '0.0.0.0'
+const PORT = 8000;
+const HOST = "0.0.0.0";
 
-const app = express()
-app.get('/', (req, res) => {
-    res.send('Hello world!\n')
-})
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Roni Ragil Iman Khoirul\n");
+});
 
-app.listen(PORT, HOST)
-console.log(`Running on http://${HOST}:${PORT}`)
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
